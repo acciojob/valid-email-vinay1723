7f9a8bf32df4c42a4971996a37c4419b02bf40c1
vinay1723 <144577819+vinay1723@users.noreply.github.com>
2025-06-25 17:18:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 function validEmail(str) {
   //your JS code here.
+	let regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
 }
 
 // Do not change the code below.
-const str = prompt("Enter an email address.");
-alert(validEmail(str));
+// const str = prompt("Enter an email address.");
+// alert(validEmail(str));
+
